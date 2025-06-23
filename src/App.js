10eda2aadca27f12d6be7e2f4686c90e668dbff2
lrfdevito <vitalytinos@gmail.com>
2025-06-24 01:37:28 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import React from "react";
-import MapComponent from "./components/MapComponent";
-
-function App() {
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <MapComponent />
-    </div>
-  );
-}
-
-export default App;
+// App component to be written next
